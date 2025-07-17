6df48116d4745139553368b3e50e22efd5a03df9
Jin-uu <jinu200131@gmail.com>
2025-07-17 15:13:20 +0900

    solved boj 11382

---
+++ 11382/11382.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using ll = long long;
+
+int main(void) {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  ll A, B, C;
+  std::cin >> A >> B >> C;
+  std::cout << A + B + C;
+}
