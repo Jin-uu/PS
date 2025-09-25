6e3c365dc9f076d8558bbbf50ee34551ad469aaf
Jin-uu <jinu200131@gmail.com>
2025-09-25 23:38:44 +0900

    solved boj 4999

---
+++ 4999/4999.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+  std::string a, b;
+  std::cin >> a >> b;
+  if(a.size() >= b.size())
+    std::cout << "go";
+    else
+    std::cout << "no";
+}
